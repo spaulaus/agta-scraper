7d0b88e7332a98982f5404765ad267c1ece7ac45
S. V. Paulauskas <Stan.Paulauskas@jtv.com>
2020-04-17 13:17:11 -0400

    Skeletons for unittests

---
+++ test/unit/test_addressline1_extraction.py
@@ -0,0 +1,24 @@
+"""
+file: test_phone_number_extraction.py
+brief:
+author: S. V. Paulauskas
+date: April 17, 2020
+"""
+SAMPLE_LIST = [
+    "4100 Birch St., Ste. 400",
+    "PO Box 7214",
+    "818 Madison Ave."
+    "7915 Minnetonka Blvd.",
+    "204 Santa Fe Trail,   Ste. 1003",
+    "909 Marina Village Pkwy.,   ",
+    "12218 Bradford Green Sq.,   Ste. 406",
+    "122 N Main St,   ",
+    "120 Broadway,   Ste. 2820",
+    "111 Stanford Shopping Center,   ",
+    "51268 Danview Technology Ct.,   ",
+    "1220 Broadway,   3rd Fl.",
+    "5622 Rogers Ave,   ",
+    "7202 Durango Cir.,   ",
+    "900 N. Michigan Ave, L5-14,   ",
+    "2044 San Marco Blvd,   "
+]
---
+++ test/unit/test_phone_number_extraction.py
@@ -0,0 +1,13 @@
+"""
+file: test_phone_number_extraction.py
+brief:
+author: S. V. Paulauskas
+date: April 17, 2020
+"""
+SAMPLE_LIST = [
+    "6464159260",
+    "212 6561437"
+    "123-456-7890"
+    "(919) 612-0710"
+    " 5129657186"
+]
